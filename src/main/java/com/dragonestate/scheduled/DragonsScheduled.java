0851e8b90ec856f0bb55c4dfcf38dab7ffdec66f
Elena <elvishn475@gmail.com>
2026-02-18 15:12:10 +0300

    add annotation @Value

--- src/main/java/com/dragonestate/scheduled/DragonsScheduled.java
+++ src/main/java/com/dragonestate/scheduled/DragonsScheduled.java
@@ -2,6 +2,7 @@ package com.dragonestate.scheduled;
 
 import com.dragonestate.repository.InMemoryDragonRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,15 +12,17 @@ import org.springframework.stereotype.Component;
 public class DragonsScheduled {
     private final InMemoryDragonRepository repository;
 
-    @Scheduled(fixedRate = 60000)
+    @Value("${dragons.hunger.fixed-rate}")
+    @Scheduled(fixedRateString = "${dragons.hunger.fixed-rate}")
     public void increasedHunger() {
         repository.findAll()
                 .forEach(dragon -> {
                     repository.updateHunger(dragon.getId(), Math.min(99, dragon.getHunger()) + 1);}); //update
     }
 
+    @Value("${dragons.age.fixed-rate}")
     @Async
-    @Scheduled(fixedRate = 300000)
+    @Scheduled(fixedRateString = "${dragons.age.fixed-rate}")
     public void increasedAge() {
         repository.findAll()
                 .forEach(dragon -> {
